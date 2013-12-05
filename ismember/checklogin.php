02ac4c8e046b16a226b0111224897247d172944e
Patrik Greco <sikevux@sikevux.se>
2013-12-06 00:17:24 +0100

    Converted the whole project to mysqli

--- ismember/checklogin.php
+++ ismember/checklogin.php
@@ -3,20 +3,20 @@ session_start();
 ob_start();
 require "local-config.php";
 
-mysql_connect("$host", "$username", "$password") or die("cannot connect");
-mysql_select_db("$db_name") or die("cannot select DB");
+($GLOBALS["___mysqli_ston"] = mysqli_connect("$host",  "$username",  "$password")) or die("cannot connect");
+((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE $db_name")) or die("cannot select DB");
 
 $myusername=$_POST['myusername'];
 $mypassword=$_POST['mypassword'];
 
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
+$myusername = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $myusername) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+$mypassword = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $mypassword) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
 $sql = "SELECT * FROM $tbl_name WHERE username='$myusername' and password=sha1('$mypassword')";
-$result = mysql_query($sql);
+$result = mysqli_query($GLOBALS["___mysqli_ston"], $sql);
 
-if (mysql_num_rows($result)) {
+if (mysqli_num_rows($result)) {
 	$_SESSION['valid'] = 1;
 	header("location:ismember.php");
 }
